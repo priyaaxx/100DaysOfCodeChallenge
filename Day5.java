09ff6480322e1e38cd96adf4de2f7272187840d8
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-05-05 09:45:51 +0530

    Trapped Rainwater
    Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.

---
+++ Day5.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Day5
+{
+    public static int trappedRainwater(int height[])
+    {
+        int leftMax[]=new int [height.length];
+        leftMax[0]=height[0];
+        for(int i=1;i<height.length;i++)
+        {
+            leftMax[i]=Math.max(height[i], leftMax[i-1]);
+        }
+
+        int rightMax[]=new int[height.length];
+        rightMax[height.length-1]=height[height.length-1];
+        for(int i=height.length-2;i>=0;i--)
+        {
+            rightMax[i]=Math.max(height[i],rightMax[i+1]);
+        }
+
+        int tw=0;
+        for(int i=0;i<height.length;i++)
+        {
+            int wl=Math.min(leftMax[i],rightMax[i]);
+            tw+=wl-height[i];
+        }
+        return tw;
+    }
+    public static void main(String args[])
+    {
+        int height[]={0, 1, 0,  2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int tw=trappedRainwater(height);
+        System.out.println("Trapped Rainwater : "+tw);
+    }
+}
