782f9b7bc0e7e1be9d940ef027fe55176068210b
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-05-13 23:10:24 +0530

    Remove element

---
+++ Day13.java
@@ -0,0 +1,15 @@
+public class Day13
+{
+    public static int removeElement(int[] nums, int val) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[ans]=nums[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
