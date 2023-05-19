f47ba40e6bc870a556e1f21a3a3be6a5abc861ed
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-05-19 22:50:06 +0530

    Contains duplicate

---
+++ Day19.java
@@ -0,0 +1,15 @@
+public class Day19
+{
+    public boolean containsDuplicate(int[] nums) {
+        int l=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<l-1;i++)
+        {
+                if(nums[i]==nums[i+1])
+                {
+                    return true;
+                }
+        } 
+        return false;  
+    }
+}
