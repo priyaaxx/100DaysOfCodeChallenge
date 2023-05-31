df7be21710d7c29a90bd419dfb6f5b309a652939
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-06-01 01:46:42 +0530

    First unique character in a string

---
+++ Day32.java
@@ -0,0 +1,25 @@
+public class Day32
+{
+    public int firstUniqChar(String s) 
+    {
+         int len = s.length();
+        
+        if(len == 0){
+            return -1;
+        }
+        
+        int[] arr = new int[26];
+        
+        for(char c : s.toCharArray()){
+            arr[c - 'a']++;
+        }
+        
+        for(int i=0; i<len; i++){
+            if(arr[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
