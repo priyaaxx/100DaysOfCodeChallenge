8136e511eaba832480a83ae718bcd62afda48310
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-06-05 23:39:56 +0530

    Friends pairing problem

---
+++ Day36.java
@@ -0,0 +1,15 @@
+public class Day36
+{
+    public static int friendsPairing(int n)
+    {
+        if(n==1 || n==2)
+        {
+            return n;
+        }
+        int fnm1=friendsPairing(n-1);
+        int fnm2=friendsPairing(n-2);
+        int pairWays=(n-1)*fnm2;
+        int totWays=fnm1+pairWays;
+        return totWays;
+    }
+}
