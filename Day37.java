5ef21dca69c09ecac2577d899da93244f263311a
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-06-06 22:03:48 +0530

    Search in rotated sorted array

---
+++ Day37.java
@@ -0,0 +1,25 @@
+public class Day37
+{
+     public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1; 
+
+        while (low <= high) { 
+            int mid = (low + high) >> 1; 
+            if (nums[mid] == target)
+                return mid; 
+
+            if (nums[low] <= nums[mid]) { 
+                if (nums[low] <= target && nums[mid] >= target)
+                    high = mid - 1; 
+                else
+                    low = mid + 1; 
+            } else { 
+                if (nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1; 
+    }
+}
