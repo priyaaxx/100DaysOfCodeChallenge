4a7f2c0a4a0ab272cf4f8e96148b652fa49cbfa9
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-06-15 22:26:50 +0530

    Excel sheet column title

---
+++ Day46.java
@@ -0,0 +1,15 @@
+public class Day46
+{
+    public String convertToTitle(int columnNumber) {
+     StringBuilder ans= new StringBuilder();
+        while(columnNumber>0)
+        {
+            --columnNumber;
+            int d= columnNumber%26;
+            columnNumber/=26;
+            ans.append((char)('A'+d));            
+        }
+        ans.reverse();
+        return ans.toString();    
+    }
+}
