8b75bf18ee8aeb75e1dfd8ca19d4100cebf2aaeb
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-06-20 22:17:25 +0530

    Construct the rectangle

---
+++ Day51.java
@@ -0,0 +1,13 @@
+public class Day51
+{
+      public int[] constructRectangle(int area) {
+        int[] ans = {area, 1};
+        for(int i=1;i*i<=area;i++){
+            if(area%i == 0 && (area/i - i)<(ans[0] - ans[1])){
+                ans[0] = area/i;
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+}
