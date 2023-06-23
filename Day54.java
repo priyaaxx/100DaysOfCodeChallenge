6c5933b383007579886799fba54df68a50aa8c5f
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-06-23 22:41:19 +0530

    Linked list cycle

---
+++ Day54.java
@@ -0,0 +1,22 @@
+public class Day54
+{
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+     public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
