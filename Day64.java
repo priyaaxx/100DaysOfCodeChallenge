7888670e8081b0237680cc2f7365916b21a2a95b
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-07-03 22:39:46 +0530

    Remove all adjacent duplicates in string

---
+++ Day64.java
@@ -0,0 +1,17 @@
+public class Day64
+{
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (final char c : s.toCharArray()) 
+        {
+            final int n = sb.length();
+            if (n > 0 && sb.charAt(n - 1) == c)
+                sb.deleteCharAt(n - 1);
+            else
+                sb.append(c);
+        }
+
+    return sb.toString();
+    }
+}
