514b16721ba53955cbab178a783a961534ffc0af
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-07-04 23:34:17 +0530

    Binary tree preorder traversal

---
+++ Day65.java
@@ -0,0 +1,29 @@
+public class Day65
+{
+     public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preorder(root, ans);
+        return ans;
+    }
+
+    private void preorder(TreeNode root, List<Integer> ans) {
+        if (root == null)
+        return;
+
+        ans.add(root.val);
+        preorder(root.left, ans);
+        preorder(root.right, ans);
+    }
+}
