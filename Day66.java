366cbe432f1a451c76f0096719b177c652acd967
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-07-05 23:17:32 +0530

    Binary tree postorder traversal

---
+++ Day66.java
@@ -0,0 +1,29 @@
+public class Day66
+{
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        postorder(root, ans);
+        return ans;
+  }
+
+    private void postorder(TreeNode root, List<Integer> ans) {
+        if (root == null)
+        return;
+
+        postorder(root.left, ans);
+        postorder(root.right, ans);
+        ans.add(root.val);
+  }
+}
