56d004d593aca1dcc67afb66fbc1b19fe2442905
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-07-06 21:20:23 +0530

    Binary tree inorder traversal

---
+++ Day67.java
@@ -0,0 +1,30 @@
+public class Day67
+{
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            list.add(stack.peek().val);
+            node = stack.pop().right;
+        }
+        return list;
+    }
+}
