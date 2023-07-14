044862698e803d2471b97c900a13735a7b688fca
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-07-14 22:06:56 +0530

    Invert binary tree

---
+++ Day75.java
@@ -0,0 +1,25 @@
+public class Day75
+{
+     public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+        return null;
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+  }
+}
