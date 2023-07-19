4e77d57362985dc49e3ddeda226c9ddec1294bf0
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2023-07-19 21:08:51 +0530

    Kth largest element in a stream

---
+++ Day80.java
@@ -0,0 +1,21 @@
+class Day80 {
+  public Day80(int k, int[] nums) {
+    this.k = k;
+    for (final int num : nums)
+      heapify(num);
+  }
+
+  public int add(int val) {
+    heapify(val);
+    return minHeap.peek();
+  }
+
+  private final int k;
+  private Queue<Integer> minHeap = new PriorityQueue<>();
+
+  private void heapify(int val) {
+    minHeap.offer(val);
+    if (minHeap.size() > k)
+      minHeap.poll();
+  }
+}
